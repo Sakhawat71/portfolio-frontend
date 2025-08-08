196e32f50dd68f28e03a864bd15c9280aa0f1c80
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-08-08 19:20:16 -0100

    remove vercel.json

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,3 @@ export { default } from "next-auth/middleware"
 export const config = { 
     matcher: ["/dashboard/:path*"] 
 };
-
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function middleware(req: NextRequest) {
-//     const token = req.cookies.get("accessToken")?.value;
-
-//     const protectedRoutes = ["/dashboard"];
-
-//     if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
-//         return NextResponse.redirect(new URL("/login", req.url));
-//     }
-
-//     return NextResponse.next();
-// }
-
-// export const config = {
-//     matcher: ["/dashboard/:path*"],
-// };
