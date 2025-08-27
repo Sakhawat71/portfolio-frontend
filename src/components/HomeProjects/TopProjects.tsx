f331bef2d32cc25883050bf7fcd7e89bf2bec18e
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-08-26 21:57:44 -1200

    project header

--- src/components/HomeProjects/TopProjects.tsx
+++ src/components/HomeProjects/TopProjects.tsx
@@ -1,9 +1,30 @@
+import Image from "next/image";
 import { ProjectSection } from "./ProjectSection";
+import lineImage from '@/assets/ext/line.png'
+
 
 const TopProjects = () => {
 
     return (
         <div>
+
+            <div>
+                <Image
+                    src={lineImage}
+                    alt='line'
+                    width={150}
+                    height={50}
+                    className='my-20 mx-auto'
+                />
+
+                <div className='my-20 flex items-center'>
+                    <div className=' border-black border-[5px] w-72 mx-auto text-center '>
+                        <h2 className='text-black font-bold text-xl tracking-[6px] p-3 uppercase '>PROJECRTS</h2>
+                    </div>
+                </div>
+            </div>
+
+
             <ProjectSection
                 name="TutorLink"
                 type="Team"
@@ -15,9 +36,9 @@ const TopProjects = () => {
                 ]}
                 work="Designed database schema, built backend API with Prisma & PostgreSQL, integrated SurjoPay payments."
                 images={[
-                    "https://i.ibb.co.com/Tq8Q0jXj/review-hub-s3h-vercel-app-1.png",
-                    "https://i.ibb.co.com/0jTHWNgD/review-hub-s3h-vercel-app-2.png",
-                    "https://i.ibb.co.com/PvmTzzQw/review-hub-s3h-vercel-app-3.png"
+                    `https://i.ibb.co.com/Tq8Q0jXj/review-hub-s3h-vercel-app-1.png`,
+                    `https://i.ibb.co.com/0jTHWNgD/review-hub-s3h-vercel-app-2.png`,
+                    `https://i.ibb.co.com/PvmTzzQw/review-hub-s3h-vercel-app-3.png`
                 ]}
                 githubFrontend="https://github.com/..."
                 githubBackend="https://github.com/..."
@@ -35,7 +56,7 @@ const TopProjects = () => {
                     "Project showcase with carousel",
                 ]}
                 work="Built with Next.js, Tailwind, shadcn/ui, and Framer Motion."
-                images={["https://i.ibb.co.com/PvmTzzQw/review-hub-s3h-vercel-app-3.png"]}
+                images={[`https://i.ibb.co.com/PvmTzzQw/review-hub-s3h-vercel-app-3.png`]}
                 githubFrontend="https://github.com/..."
                 live="https://portfolio.com"
                 reverse={true}
