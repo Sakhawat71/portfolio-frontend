1ede6e6f6f9e7cdc553cbc8a3ed70340d05fb5c2
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-09-13 14:01:47 +0600

    add about me and delete home blog card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner/Banner";
+import AboutMe from "@/components/home/AboutMe";
 import HomeBlogs from "@/components/HomeBlogs/HomeBlogs";
 import TopProjects from "@/components/HomeProjects/TopProjects";
 import Skills from "@/components/Skills/Skills";
@@ -8,6 +9,7 @@ const HomePage = () => {
     return (
         <div className="bg-[#D7D7D7]">
             <Banner />
+            <AboutMe />
             <Skills />
             <TopProjects />
             <HomeBlogs />
---
+++ src/components/home/AboutMe.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div className='bg-[#1D1D1D] px-10 md:px-20 py-10 text-white'>
+            <h2 className='mb-5 uppercase'>who am i</h2>
+            <p className='font-sans'>
+                I&apos;m <strong>Sakhawat H</strong>, a passionate <strong>Backend</strong> and <strong>Full-Stack Developer</strong> with a solid background in Computer Science and Engineering.
+                I specialize in building scalable, efficient, and secure systems using <strong>Node.js</strong>, <strong>Express.js</strong>, <strong>PostgreSQL</strong>, <strong>Prisma</strong>, <strong>MongoDB</strong>, and <strong>Mongoose</strong>, while also crafting dynamic front-ends with <strong>React.js</strong> and <strong>Next.js</strong>.
+                With strong experience in the <strong>MERN</strong> stack, I love solving complex problems, optimizing performance, and bringing ideas to life through clean and modern web solutions.
+            </p>
+        </div>
+    );
+};
+
+export default AboutMe;
