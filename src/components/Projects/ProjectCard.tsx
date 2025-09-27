9210770ab7e82ea8e67296d0290275cf6e7ba65a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-09-27 20:54:42 +0600

    Refactor ProjectCard component: update title truncation, replace Github icon with Telescope, and enhance button styling

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -3,7 +3,7 @@
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ExternalLink, Github } from "lucide-react";
+import { ExternalLink, Telescope } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { IProject } from "@/types/project.type";
@@ -15,68 +15,69 @@ const ProjectCard = (
 
     return (
         <Card className="rounded-2xl shadow-md hover:shadow-xl transition duration-300 bg-[#E7F2E4] hover:scale-[1.02]">
-            {/* <Link href={`/projects/${project.id}`} className="block"> */}
-                {/* Image */}
-                <CardHeader className="p-0 relative">
-                    <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={600}
-                        height={400}
-                        className="rounded-t-2xl h-52  w-full"
-                    />
-                    <span className="absolute top-3 right-3 bg-black/60 text-white text-xs px-2 py-1 rounded">
-                        {project.isTeam ? `Team (${project.teamSize})` : "Solo"}
-                    </span>
-                </CardHeader>
+            {/* Image */}
+            <CardHeader className="p-0 relative">
+                <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={600}
+                    height={400}
+                    className="rounded-t-2xl h-52  w-full"
+                />
+                <span className="absolute top-3 right-3 bg-black/60 text-white text-xs px-2 py-1 rounded">
+                    {project.isTeam ? `Team (${project.teamSize})` : "Solo"}
+                </span>
+            </CardHeader>
 
-                {/* Content */}
-                <CardContent className="space-y-3 p-5">
-                    <div className="flex justify-between items-center">
-                        <h3 className="text-lg font-semibold text-gray-900">{project.title}</h3>
-                        <Link href={project.liveUrl} target="_blank">
-                            <Button variant="link" className="gap-1 text-sm">
-                                <ExternalLink className="h-4 w-4" /> Live
-                            </Button>
-                        </Link>
-                    </div>
-                    <p className="text-sm text-gray-600 line-clamp-3">
-                        {project?.description?.slice(0,45)} ....
+            {/* Content */}
+            <CardContent className="space-y-3 p-5">
+                <div className="flex justify-between items-center">
+                    <h3 className="text-lg font-semibold text-gray-900">
+                        {
+                            project?.title?.length <= 20 ? project?.title :
+                                project?.title?.slice(0, 20) + '...'
+                        }
+                    </h3>
+                    <Link href={project.liveUrl} target="_blank">
+                        <Button variant="link" className="gap-1 text-sm">
+                            <ExternalLink className="h-4 w-4" /> Live
+                        </Button>
+                    </Link>
+                </div>
+                <p className="text-sm text-gray-600 line-clamp-3">
+                    {
+                        project?.description?.length <= 40
+                            ? project?.description
+                            : project?.description?.slice(0, 40) + '...'
+                    }
+                </p>
+
+                {/* Tech stack */}
+                <div className="flex flex-wrap gap-2 mt-3">
+                    {project.techStack.map((tech) => (
+                        <Badge key={tech} className="bg-gray-100 text-gray-700 text-xs">
+                            {tech}
+                        </Badge>
+                    ))}
+                </div>
+
+                {/* Role + Duration */}
+                {project.roleInTeam && (
+                    <p className="text-xs text-gray-500">
+                        Role: {project.roleInTeam} | {project.startDate} - {project.endDate}
                     </p>
-
-                    {/* Tech stack */}
-                    <div className="flex flex-wrap gap-2 mt-3">
-                        {project.techStack.map((tech) => (
-                            <Badge key={tech} className="bg-gray-100 text-gray-700 text-xs">
-                                {tech}
-                            </Badge>
-                        ))}
-                    </div>
-
-                    {/* Role + Duration */}
-                    {project.roleInTeam && (
-                        <p className="text-xs text-gray-500">
-                            Role: {project.roleInTeam} | {project.startDate} - {project.endDate}
-                        </p>
-                    )}
-                </CardContent>
-            {/* </Link> */}
+                )}
+            </CardContent>
 
             {/* Footer */}
-
-            {/* <CardFooter className="flex justify-between items-center px-4 pb-4">
-                <Link href={project.liveUrl} target="_blank">
-                    <Button variant="outline" className="gap-1 text-sm">
-                        <ExternalLink className="h-4 w-4" /> Live
-                    </Button>
-                </Link>
+            <CardFooter className="flex justify-center items-center px-4 pb-4">
                 <Link href={`/projects/${project.id}`} >
-                    <Button variant="outline" className="gap-1 text-sm">
-                        <Github className="h-4 w-4" />
-                         Details
+                    <Button variant="secondary" className="gap-1 text-sm">
+                        <Telescope className="h-4 w-4" />
+                        Explore Project
                     </Button>
                 </Link>
-            </CardFooter> */}
+            </CardFooter>
 
         </Card>
     );
