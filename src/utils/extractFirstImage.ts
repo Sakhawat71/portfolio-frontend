1024b6e3fdb173bc56bc364211874b0c09e17e30
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-10-01 23:56:48 +0600

    blog redesign

--- src/utils/extractFirstImage.ts
+++ src/utils/extractFirstImage.ts
@@ -3,12 +3,6 @@ export const extractFirstImageUrlFromHtml = (html: string): string | null => {
     return match ? match[1] : null;
 };
 
-// export const extractFirstParagraphFromHtml = (html: string): string | null => {
-//     const match = html.match(/<p[^>]*>(.*?)<\/p>/i);
-//     if (!match) return null;
-//     const textOnly = match[1].replace(/<[^>]+>/g, "");
-//     return textOnly.trim();
-// };
 
 export const extractFirstParagraphFromHtml = (html: string): string | null => {
     const matches = html.match(/<p[^>]*>(.*?)<\/p>/gi);
