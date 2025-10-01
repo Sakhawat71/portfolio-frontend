3ccda73af0e9c98c554e3238954fae9e09d507ff
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-10-01 23:30:32 +0600

    extractFirstParagraphFromHtml fixed

--- src/utils/extractFirstImage.ts
+++ src/utils/extractFirstImage.ts
@@ -3,9 +3,22 @@ export const extractFirstImageUrlFromHtml = (html: string): string | null => {
     return match ? match[1] : null;
 };
 
+// export const extractFirstParagraphFromHtml = (html: string): string | null => {
+//     const match = html.match(/<p[^>]*>(.*?)<\/p>/i);
+//     if (!match) return null;
+//     const textOnly = match[1].replace(/<[^>]+>/g, "");
+//     return textOnly.trim();
+// };
+
 export const extractFirstParagraphFromHtml = (html: string): string | null => {
-    const match = html.match(/<p[^>]*>(.*?)<\/p>/i);
-    if (!match) return null;
-    const textOnly = match[1].replace(/<[^>]+>/g, "");
-    return textOnly.trim();
+    const matches = html.match(/<p[^>]*>(.*?)<\/p>/gi);
+    if (!matches) return null;
+
+    for (const p of matches) {
+        const textOnly = p.replace(/<[^>]+>/g, "").trim();
+        if (textOnly.length > 0) {
+            return textOnly;
+        }
+    };
+    return null;
 };
